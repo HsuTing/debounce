42e26de36cf6bd7cb184c154608fe098e019c138
HsuTing <hsuting0106@gmail.com>
2022-05-19 21:15:26 +0800

    feat(debounce): add new function

---
+++ debounce.js
@@ -0,0 +1,10 @@
+module.exports = (callback, delay) => {
+  let timer;
+
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
